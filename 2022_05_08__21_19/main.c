e2787e886389b0235002a078e75cc7755c91705b
L15315192513 <15315192513@189.cn>
2022-05-08 22:50:14 +0800

    第一个C语言程序

---
+++ 2022_05_08__21_19/main.c
@@ -0,0 +1,9 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>				/*引用的 stdio.h 头文件*/
+
+int main()						/*main 主函数*/
+{
+	printf("Hello,World!\n");	/*printf 打印输出 Hello,World!*/
+
+	return 0;					/*返回值 0*/
+}
